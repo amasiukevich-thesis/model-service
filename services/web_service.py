61de93f089d1e065f911353c02a11334f76c30c4
amasiukevich <anton.masiukiewicz1@gmail.com>
2023-07-30 18:19:20 +0200

    Little refactoring for helpers.py

--- services/web_service.py
+++ services/web_service.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel
 
-from services.helpers import (
+from src.helpers import (
     PricePredictionService,
-    load_model,
     TooFarFutureException
 )
 
